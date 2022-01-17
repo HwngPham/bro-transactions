497623e13a28663a9b3dd0fcf44d3da57904f419
hungpq96 <quanghwng22@gmail.com>
2022-01-17 19:26:03 +0700

    Allow register name

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -22,15 +22,18 @@ def log_in(request):
 
 def register(request):
     email = request.POST.get('email')
+    username = request.POST.get('username')
     password = request.POST.get('password')
     password_repeat = request.POST.get('password_repeat')
 
     if request.method == 'POST':
+        # TODO: abstract validation into validators
         if not email or not password or not password_repeat:
             messages.error(request, 'Invalid credentials!')
             return redirect('/register')
 
-        user_already_existed = len(User.objects.filter(email=email)) > 0
+        user_already_existed = len(User.objects.filter(email=email)) > 0 \
+            or len(User.objects.filter(username=username)) > 0
         if user_already_existed:
             messages.error(request, 'The user already exist!')
             return redirect('/register')
@@ -39,11 +42,14 @@ def register(request):
             messages.error(request, 'Password repeat does not match!')
             return redirect('/register')
 
-        user = User()
-        user.email = email
-        user.password = make_password(password)
-        user.username = email.split("@")[0]
+        user = User(
+            email=email,
+            username=username,
+            password=make_password(password),
+        )
         user.save()
+
+        login(request, user)
         return redirect('homepage')
     else:
         return render(request, 'accounts/register.html')
